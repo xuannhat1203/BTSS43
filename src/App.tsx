07a810d9b1d753af77e4f56ae3e53d622e29dcdf
xuannhat <xuannhatvn211@gmail.com>
2024-05-24 11:26:21 -0700

    fix

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Admin from "./Components/Admin";
+import Register from "./Components/Register";
+
+export default function App() {
+  return (
+    <div>
+      <Register />
+      <Admin />
+    </div>
+  );
+}
